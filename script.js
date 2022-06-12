9e67c09569b52fcea90bf678e5863152b36b4fa1
kLovepreet161 <88380793+kLovepreet161@users.noreply.github.com>
2022-06-12 12:21:59 +0930

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('min');
+const secEl = document.getElementById('sec');
+
+const newYears = '1 Jan 2023';
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+
+    const seconds = Math.floor(totalSeconds % 60);
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    if (time < 10) {
+        return `0${time}`;
+    } else{
+     return time;
+    }
+}
+
+// Intial call
+countdown();
+setInterval(countdown, 1000);
